9f3bcfea7694aac3eb1efddc8d45b70bd95bc10e
nugugae <89291329+nugugae@users.noreply.github.com>
2023-09-28 15:31:43 +0900

    update

--- sogeun/src/main/java/com/team14/sogeun/SogeunApplication.java
+++ sogeun/src/main/java/com/team14/sogeun/SogeunApplication.java
@@ -2,8 +2,9 @@ package com.team14.sogeun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SogeunApplication {
 
 	public static void main(String[] args) {
