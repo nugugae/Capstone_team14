04ef0db38f76729ef0387e144b625743231f4dbb
Lisa-Tera <56185149+Lisa-Tera@users.noreply.github.com>
2023-10-05 23:32:54 +0900

    Update creat.sql

--- SqlEx/creat.sql
+++ SqlEx/creat.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (
+CREATE TABLE web_users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     login_id VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -7,19 +7,19 @@ CREATE TABLE users (
     UNIQUE KEY (login_id)
 );
 
-CREATE TABLE questions (
+CREATE TABLE gpt_questions (
     question_Id INT AUTO_INCREMENT PRIMARY KEY,
     id INT,
     emotion VARCHAR(255) NOT NULL,
     question TEXT,
     question_date DATETIME NOT NULL,
-    FOREIGN KEY (id) REFERENCES users(id)
+    FOREIGN KEY (id) REFERENCES web_users(id)
 );
 
-CREATE TABLE answers (
+CREATE TABLE user_answers (
     answer_Id INT AUTO_INCREMENT PRIMARY KEY,
     question_Id INT, 
     answer_date DATETIME NOT NULL,
     answer TEXT NOT NULL,
-    FOREIGN KEY (question_Id) REFERENCES questions(question_Id)
+    FOREIGN KEY (question_Id) REFERENCES gpt_questions(question_Id)
 );
