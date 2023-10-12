85fca7db7a7391a79512516c573e2f5c84e706a8
Lisa-Tera <eun01632@naver.com>
2023-10-12 10:02:46 +0900

    new_sql
    감정,qna,유저로 바꿈

---
+++ sql_new/creat.sql
@@ -0,0 +1,23 @@
+CREATE TABLE webUser(
+    uid INT AUTO_INCREMENT PRIMARY KEY,
+    loginId VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    nickname VARCHAR(100) NOT NULL UNIQUE,
+    role VARCHAR(255) DEFAULT 'USER'
+);
+CREATE TABLE qna (
+    qnaId INT AUTO_INCREMENT PRIMARY KEY,
+    uid INT,
+    question TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    qnaDate DATETIME NOT NULL,
+    FOREIGN KEY (uid) REFERENCES webUser(uid)
+);
+CREATE TABLE emotion (
+    emotion VARCHAR(100) NOT NULL,
+	uid INT,
+	emotionDate DATETIME NOT NULL,
+	FOREIGN KEY (uid) REFERENCES webUser(uid)
+);
+CREATE index qna_ix1 on qna (uid, qnaDate);
+CREATE index emotion_ix1 on emotion (uid, emotionDate);
