54902244b8d0257a6fadcad729793d60fbc17e05
nugugae <89291329+nugugae@users.noreply.github.com>
2023-10-20 15:28:34 +0900

    중간 demo

--- springweb/src/main/resources/static/js/chat.js
+++ springweb/src/main/resources/static/js/chat.js
@@ -1,68 +1,117 @@
-const API_URL = 'https://api.openai.com/v1/chat/completions';
-const API_KEY = "API_KEY";
 
-const userInput = document.getElementById("user-input");
-const sendButton = document.querySelector(".submit-button");
-const chatLog = document.querySelector(".chat-log");
-const resultContainer = document.getElementById('resultContainer');
+        const API_URL = 'https://api.openai.com/v1/chat/completions';
+        const API_KEY = "API_KEY";
 
-// 사용자가 메시지를 입력할 때마다 버튼을 활성화
-userInput.addEventListener('input', () => {
-    if (userInput.value.trim() !== '') {
-        sendButton.disabled = false;
-    } else {
-        sendButton.disabled = true;
-    }
-});
+        const userInput = document.getElementById("user-input");
+        const sendButton = document.querySelector(".submit-button");
+        const chatLog = document.querySelector(".chat-log");
+        const resultContainer = document.getElementById('resultContainer');
 
-sendButton.addEventListener('click', () => {
+        const system_prompt = [
+            {"role": "system", "content": "You empathize with users and help the user take care of their minds."},
+            {"role": "system", "content": "If you don't understand what the user says, ask again carefully."},
+            {"role": "system", "content": "If you can guess what users feel, share the guess with the user."},
+            {"role": "system", "content": "Before suggesting advice, you should ask if the user wants."},
+            {"role": "system", "content": "You should give advice with numbering."},
+            {"role": "system", "content": "If the user experiences an emotion, ask the user why they are feeling that emotion."},
+            {"role": "system", "content": "You must say it in 50 words."},
+            {"role": "system", "content": "You must say it in Korean."},
+        ]
 
-    if (!userInput.value) {
-        alert("Please enter a message.");
-        return;
-    }
+        const conversation = [
+            {"role": "assistant", "content": "오늘 하루는 어땠나요?"},
+        ]
 
-    const userMessage = userInput.value;
-    userInput.value = "";
+        const generate = async () => {
+            const userMessage = userInput.value;
+            userInput.value = "";
+            sendButton.disabled = true;
+            userInput.disabled = true;
 
-    chatLog.innerHTML += `<div class="user-message">${userMessage}</div>`;
-    
-    sendButton.disabled = true;
+            chatLog.innerHTML += `<div class="user-message">${userMessage}</div>`;
+            conversation.push({"role": "user", "content": userMessage});
+            userInput.value = "";
+            let botMessage = "";
 
-    userInput.disabled = true;
+            const response = await fetch(API_URL, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${API_KEY}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    "model": "gpt-3.5-turbo",
+                    "messages": system_prompt.concat(conversation),
+                    max_tokens: 1024,
+                    stream: true,
+                }),
+            });
 
-    // "Generating..." message 
-    chatLog.innerHTML += '<div class="chatbot-message">Generating...</div>';
+            /*.then(response => response.json())
+            .then(data => {
+            const botMessage = data.choices[0].message.content;
 
-    // GPT-3 API 호출
-    fetch(API_URL, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${API_KEY}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "model": "gpt-3.5-turbo",
-            "messages": [
-                {"role": "system", "content": "You have to provide mental care to user.answer in 50 words"},
-                {"role": "user", "content": userMessage}
-            ]
-        }),
-    })
-    .then(response => response.json())
-    .then(data => {
-        const botMessage = data.choices[0].message.content;
+            const chatbotMessages = chatLog.querySelectorAll('.chatbot-message');
+            chatbotMessages[chatbotMessages.length - 1].remove();
 
-        // "Generating..." message 삭제
-        const chatbotMessages = chatLog.querySelectorAll('.chatbot-message');
-        chatbotMessages[chatbotMessages.length - 1].remove();
+            chatLog.innerHTML += `<div class="chatbot-message">${botMessage}</div>`; */
 
-        chatLog.innerHTML += `<div class="chatbot-message">${botMessage}</div>`;
 
-        // Scroll
-        chatLog.scrollTop = chatLog.scrollHeight;
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder("utf-8");
 
-        userInput.disabled = false;
+            const chatbotMessageDiv = document.createElement('div');
+            chatbotMessageDiv.className = 'chatbot-message';
+            chatLog.appendChild(chatbotMessageDiv);
+
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) {
+                    break;
+                }
+
+                const chunk = decoder.decode(value);
+                const lines = chunk.split("\n");
+
+                const parsedLines = lines
+                    .map((line) => line.replace(/^data: /, "").trim())
+                    .filter((line) => line !== "" && line !== "[DONE]")
+                    .map((line) => JSON.parse(line));
+
+                for (const parsedLine of parsedLines) {
+
+                    const { choices } = parsedLine;
+                    const { delta } = choices[0];
+                    const { content } = delta;
+                    if (content) {
+                        botMessage += content;
+                        chatbotMessageDiv.textContent += content;
+                    }
+
+                }
+
+            }
+
+            console.log(botMessage);
+            conversation.push({"role":"assistant", "content": botMessage});
+
+
+                // Scroll
+                chatLog.scrollTop = chatLog.scrollHeight;
+                userInput.disabled = false;
+                sendButton.disabled = false;
+        };
+
+
+
+
+    sendButton.addEventListener('click',() => {
+        if (userInput.value.trim() !== '') {
+                sendButton.disabled = false;
+                generate();
+
+            } else {
+                sendButton.disabled = true;
+            }
+
     });
-
-});
